ca3a3b3613925a4b960fee1095b23c0203780b73
meflores <meflores.seiza@bancochile.cl>
2025-01-26 20:49:02 -0300

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './component/login/login.component';
+import {MisMonedasComponent} from './component/mis-monedas/mis-monedas.component';
+import {MisBilletesComponent} from './component/mis-billetes/mis-billetes.component';
+import {PaisesDelMundoComponent} from './component/paises-del-mundo/paises-del-mundo.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'mis-monedas', component: MisMonedasComponent },
+  { path: 'mis-billetes', component: MisBilletesComponent },
+  { path: 'paises-del-mundo', component: PaisesDelMundoComponent }
+];
